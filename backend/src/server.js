07e9f8137f0f79869e13d16e5e8ad328edc72cf2
Wendeley Fernandes <wendeleyti@gmail.com>
2019-10-12 14:50:04 -0300

    Add frontend da aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 const app = express();
+const path = require("path");
 
 mongoose.connect(
   "mongodb+srv://wendeleyf:wendeleyf@cluster0-9wrfi.mongodb.net/semana09?retryWrites=true&w=majority",
@@ -13,7 +15,9 @@ mongoose.connect(
 );
 
 // Indicando para o express utilizar o json
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
